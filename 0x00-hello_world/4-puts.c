33cc7a8af95e9629230446dba5d78370977c09ca
thinktank01 <felixayot@gmail.com>
2023-03-17 02:07:16 +0300

    Upload C Hello World project

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main(void)
+/**
+*main - Entry point
+*return: Always 0 (success)
+*/
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+Solutions to 0x00. C - Hello, World Project Tasks
