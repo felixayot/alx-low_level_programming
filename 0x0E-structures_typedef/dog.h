6649389c6549fc5a84efb3ee776cff27ce4d0656
felix_ayot <95885378+felixayot@users.noreply.github.com>
2023-05-09 16:41:01 +0300

    Update dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -13,15 +13,17 @@ char *name;
 float age;
 char *owner;
 };
-void init_dog(struct dog *d, char *name, float age, char *owner);
-void print_dog(struct dog *d);
+
 typedef struct
 {
 char *name;
 float age;
 char *owner;
-}
-dog_t;
+} dog_t;
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
+
 #endif
