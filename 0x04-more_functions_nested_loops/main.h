eed9fab901162c27886d7620bf6a5f5c3fe47dc3
thinktank01 <felixayot@gmail.com>
2023-06-02 17:25:03 +0300

    More corrections

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,3 +1,8 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
+#include <stdio.h>
+
 int _putchar(char c);
 int _isupper(int c);
 int _isdigit(int c);
@@ -10,3 +15,5 @@ void print_diagonal(int n);
 void print_square(int size);
 void print_triangle(int size);
 void print_number(int n);
+
+#endif
